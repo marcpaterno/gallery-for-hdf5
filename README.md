0970899ba6ca023ccd358162d3fe515c73e66d64
Marc Paterno <paterno@fnal.gov>
2017-01-06 16:49:29 -0600

    Widespread cleanup

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # gallery-demo
 
-This repository contains my code demonstrating the use of `gallery`, a UPS product for the reading of `art/ROOT` data files.
+This is the demonstration repository for `gallery`, a package for the reading of `art/ROOT` data files. Please see http://art.fnal.gov/gallery for an overview of `gallery`.
+
+
+## When to use `gallery`
+
+`gallery` provides library (as opposed to framework) facilities for reading (not writing) event data from _art_/ROOT data files. As such, it is suitable for making programs that perform tasks that do not require the full set of features of the _art_ framework. Such tasks include things like reading event data to create histograms or ntuples.
+
+Because it allows use of your experiment's event data product classes, you can use `gallery` to write code that is easy to move into your experiment's code base. The access to event data in `gallery` is very similar to that in _art_. The `gallery::Event` interface provides the same `getByLabel`, _etc_., as is present in the `art::Event` class.
+
+However, `gallery` is not a full-featured framework. In fact, it is not a framework at all. Thus is it not appropriate for tasks that require framework facilities, including:
+* access to non-event data,
+* writing of _art_/ROOT data files, and
+* use of "services" that are framework-dependent.
+
+## Running the demonstrations
+
+There are three different demonstration codes:
+
+1. `demo.cc` is a C++ main program which processes an input file and creates a ROOT histogram file. The provided `makefile` can be used to build the executable `demo` from `demo.cc`
+2. `demo.C` is a ROOT macro intended to be invoked within an interactive ROOT session, and which creates a few histograms in memory.
+3. `demo.py` is a PyROOT program that processes an input file and creates a ROOT histogram file. It can be run with `python demo.py`
--- demo.C
+++ demo.C
@@ -1,3 +1,5 @@
+#include <chrono>
+#include <functional>
 #include <iostream>
 #include <stdlib.h>
 #include <string>
@@ -21,19 +23,24 @@
 using namespace art;
 using namespace std;
 
-void demo() {
+void demo(std::string const& filename) {
 
   InputTag mctruths_tag { "generator" };
   InputTag vertex_tag { "linecluster" };
   InputTag assns_tag  { "linecluster" };
-  vector<string> filenames { "dune.root" }; // multiple files are allowed.
+  vector<string> filenames(1, filename);
 
   // Don't do this in compiled C++. We're relying on the interactive
   // ROOT system to own the histograms we create on the heap.
   auto npart_hist = new TH1F("npart", "Number of particles per MCTruth", 51, -0.5, 50.5);
   auto nclust_hist = new TH1F("nclus", "Number of clusters per vertex", 51, -0.5, 50.5);
 
+  auto  start_time = chrono::system_clock::now();
+  vector<chrono::microseconds> times; // we'll record the time for each event.
+
   for (gallery::Event ev(filenames) ; !ev.atEnd(); ev.next()) {
+    auto const t0 = chrono::system_clock::now();
+
     auto const& mctruths = *ev.getValidHandle<vector<simb::MCTruth>>(mctruths_tag);
     if (!mctruths.empty()) npart_hist->Fill(mctruths[0].NParticles());
 
@@ -46,5 +53,17 @@ void demo() {
       clusters_for_vertex.get(i, clusters);
       nclust_hist->Fill(clusters.size());
     }
+    
+    times.push_back(chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now()-t0));
   }
+  auto const elapsed_time = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start_time);
+  auto const sum_times = accumulate(begin(times), end(times), chrono::microseconds(0));
+  
+  cout << "Processed " << times.size()
+       << " events in an average of "
+       <<  sum_times.count()/times.size()
+       << " microseconds/event\n";
+  cout << "Total processing time (including file opening) was "
+       << elapsed_time.count()
+       << " milliseconds\n";
 }
